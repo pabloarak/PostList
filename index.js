5d661f7c80ffc6c244386bb805872ee27c7e202f
pabloarak <pablo.arak.vm@gmail.com>
2018-11-03 18:12:29 -0300

    incorporated JSON PlaceHolder API

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import List from './src/postlist/components/list';
-import data from './src/api.json';
 
 const app = document.getElementById('app');
 
-ReactDOM.render(<List data={data}/>,app);
+ReactDOM.render(<List />,app);
--- src/postlist/components/list.js
+++ src/postlist/components/list.js
@@ -1,20 +1,53 @@
-import React from "react";
+import React, { Component } from "react";
 import Post from "./post";
 import './list.css';
 
-function List(props) {
-  return (
-    <div className="list">
-      <div className="post-row">
-        <div className="post-cell">Name</div>
-        <div className="post-cell">Description</div>
-        <div className="post-cell">Action</div>
+class List extends Component {
+
+  state = {
+      posts: []
+  };
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then(response => response.json())
+      .then(json => {
+
+        // create an array of contacts only with relevant data
+        const newPosts = json.map(post => {
+          return {
+            id: post.id,
+            name: post.title,
+            description: post.body
+          };
+        });
+
+        // create a new "State" object without mutating
+        // the original State object.
+        const newState = Object.assign({}, this.state, {
+          posts: newPosts
+        });
+
+        // store the new state object in the component's state
+        this.setState(newState);
+      })
+      .catch(error => console.log(error));
+  }
+
+  render() {
+    return (
+      <div className="list">
+        <div className="post-row">
+          <div className="post-cell">Name</div>
+          <div className="post-cell">Description</div>
+          <div className="post-cell">Action</div>
+        </div>
+        {
+          this.state.posts.map(post => <Post key={post.id} name={post.name} description={post.description}/>)
+        }
       </div>
-      {
-        props.data.posts.map(post => <Post key={post.id} name={post.name} description={post.description}/>)
-      }
-    </div>
-  );
+    );
+  }
 }
 
 export default List;
