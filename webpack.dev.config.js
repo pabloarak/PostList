eeb79d51f0caac9b9d427956ed94e28016db7c47
pabloarak <pablo.arak.vm@gmail.com>
2019-03-11 16:11:47 -0300

    Post List 1.0.1

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -14,8 +14,6 @@ module.exports = {
   module: {
     rules: [
       {
-        // test: que tipo de archivo quiero reconocer,
-        // use: que loader se va a encargar del archivo
         test: /\.(js|jsx)$/,
         exclude: /(node_modules)/,
         use: {
